68d443bddff784a54072df047671371ea7803859
prakharjain31 <prakharjain3103@gmail.com>
2022-03-10 20:46:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/clockapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.clockapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViewPager();
+    }
+
+    public void initViewPager() {
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.setFragments(new Fragment(R.layout.fragment_alarms) , "Alarm");
+        viewPagerAdapter.setFragments(new Fragment(R.layout.fragment_timer) , "Timer");
+        viewPagerAdapter.setFragments(new Fragment(R.layout.fragment_stopwatch) , "Stopwatch");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        ArrayList<Fragment> fragments;
+        ArrayList<String> titles;
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        public void setFragments(Fragment fragment , String title) {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+
+}
